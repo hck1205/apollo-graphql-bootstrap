c653752d8c7a41c1eea05f229fef1a63c15debb3
CkHong <hck1205@daum.net>
2019-04-06 19:45:10 +0900

    added react and react route

---
+++ frontend/src/pages/Bookings.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Bookings extends Component {
+  render() {
+    return (
+      <h1>The Bookings Page</h1>
+    );
+  }
+}
+
+export default Bookings
